bee49faa164e602a69b7aed15ffee901bebb31fa
Phill_Lima <81516526+a036020@users.noreply.github.com>
2022-01-10 23:36:19 +0000

    revisao da bd

--- DockerAPI/db/dbapi.sql
+++ DockerAPI/db/dbapi.sql
@@ -1,15 +1,30 @@
 CREATE SCHEMA `api`;
 use `api`;
+
+CREATE TABLE `livro`(
+  `id` bigint not null AUTO_INCREMENT,
+  `nome` varchar(32) NOT NULL,
+  `edicao` int,
+  `paginas` int,
+  `genero` varchar(32) NOT NULL,
+  `editora` varchar(32),
+  `escritor` varchar(32) NOT NULL,
+  `lido` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB;
+
 CREATE TABLE `filme`(
   `id` bigint not null AUTO_INCREMENT,
   `nome` varchar(32) NOT NULL,
   `ano` int NOT NULL,
-  `duracao` varchar(32),
+  `duracao` int,
   `genero` varchar(32) NOT NULL,
   `produtora` varchar(32),
   `diretor` varchar(32),
   `assistido` varchar(32) NOT NULL,
   `adptado` bigint,
+  KEY `adptado_fk_` (`adptado`),
+  CONSTRAINT `adptado_fk` FOREIGN KEY (`adptado`) REFERENCES `livro` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB;
 
@@ -23,18 +38,6 @@ CREATE TABLE `serie`(
     `assistido` varchar(32) NOT NULL,
     PRIMARY KEY (`id`)
   ) ENGINE=INNODB; 
-  
-  CREATE TABLE `livro`(
-    `id` bigint not null AUTO_INCREMENT,
-    `nome` varchar(32) NOT NULL,
-    `edicao` int,
-    `paginas` int,
-    `genero` varchar(32) NOT NULL,
-    `editora` varchar(32),
-    `escritor` varchar(32) NOT NULL,
-    `lido` varchar(32) NOT NULL,
-    PRIMARY KEY (`id`)
-  ) ENGINE=INNODB;
   
   CREATE TABLE `curso`(
     `id` bigint not null AUTO_INCREMENT,
